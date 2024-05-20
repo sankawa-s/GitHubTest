f66b60465a1448a6710995fdc62ce231291f15bb
sankawa-s <163833745+sankawa-s@users.noreply.github.com>
2024-05-20 17:49:14 +0900

    Update register_to_clb.sh

--- src/scripts/register_to_clb.sh
+++ src/scripts/register_to_clb.sh
@@ -4,7 +4,10 @@
 CLB_NAME="sankawa-deploytest"
 
 # インスタンスIDを取得
-INSTANCE_ID=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+INSTANCE_ID=$(TOKEN=`curl -X PUT "http://169.254.169.254/latest/api/token" \
+  -H "X-aws-ec2-metadata-token-ttl-seconds: 21600"` \
+&& curl -H "X-aws-ec2-metadata-token: $TOKEN" \
+    -v http://169.254.169.254/latest/meta-data/instance-id/)
 
 # AWS CLIを使ってインスタンスをCLBに登録
 aws elb register-instances-with-load-balancer --load-balancer-name $CLB_NAME --instances $INSTANCE_ID
