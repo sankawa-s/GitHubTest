c26b1b278c644537daef08a5486fd7e1bbc51cb5
sankawa-s <163833745+sankawa-s@users.noreply.github.com>
2024-05-21 10:53:03 +0900

    Delete src/scripts/configure_application.sh

--- src/scripts/configure_application.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Copy configuration files
-cp /path/to/config/file /path/to/application/config/file
-
-# Set environment variables
-export APP_ENV=production
-
-echo "Application configured successfully."
